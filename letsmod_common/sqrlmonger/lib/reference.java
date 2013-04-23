5819f13a2371c066f7e7c2740928e73b6b82d2cc
Sqrlmonger <Sqrlmonger@gmail.com>
2013-04-23 00:23:45 -0500

    First commit

---
+++ letsmod_common/sqrlmonger/lib/reference.java
@@ -0,0 +1,9 @@
+package sqrlmonger.lib;
+
+public class reference {
+    
+    public static final String MOD_ID = "LetsMod";
+    public static final String NAME = "Let's Mod";
+    public static final String VERSION = "0.0.1";
+    
+}
